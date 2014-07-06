79d03c086f43e78203727265574821a6dd00bdb8
Zachery <zbonzo@gmail.com>
2014-07-06 06:20:16 -0700

    Main files
    Adding the 3 main files

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var clicks = 0;
+
+function incrementby(number){
+	clicks =  clicks + number;
+		document.getElementById("clicks").innerHTML = clicks;
+};
+		
+
+window.setInterval(function(){
+incrementby(1);
+},1000);
+
+		
