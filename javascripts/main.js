a2f67ea8aeb0cbce8e1478097a0273ee1fe3972d
zbonzo <zbonzo@gmail.com>
2014-07-06 07:28:04 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+var clicks = 0;
+
+function incrementby(number){
+clicks = clicks + number;
+document.getElementById("clicks").innerHTML = clicks;
+};
+
+
+window.setInterval(function(){
+incrementby(1);
+},1000);
+
