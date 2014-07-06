0c3ed21c2fd0fc8c248ef1928739537ba161f3e9
zbonzo <zbonzo@gmail.com>
2014-07-06 09:36:24 -0700

    Update main.js
    Commenting out the last two parts of the local storage until I figure out what to do with them

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,12 +4,14 @@ var standardInterval = 1000;
 var moneyIncrementInterval = 7;
 
 if (localStorage.getItem("intMoney")){
-    /*
+    
     intMoney = localStorage.getItem("intMoney");
     intDate = localStorage.getItem("intDate");
-    standardInterval = localStorage.getItem("standardInterval");
-    moneyIncrementInterval = localStorage.getItem("moneyIncrementInterval");
-    */
+    
+    //This is commented out until I figure out what I want to do with it.
+    //standardInterval = localStorage.getItem("standardInterval");
+    //moneyIncrementInterval = localStorage.getItem("moneyIncrementInterval");
+    
 } 
 
 
