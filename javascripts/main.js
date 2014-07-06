61428133fd2113b1a392a82f13f58be2de979368
zbonzo <zbonzo@gmail.com>
2014-07-06 08:42:34 -0700

    Update main.js
    Set everything back to normal.  Added variables to make code more fluid(?).

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 var Money = 0;
 var Date = 0;
-
+var standardInterval = 1000;
+var moneyIncrementInterval = 7;
 
 function incrementMoneyBy(number){
 Money = Money + number;
@@ -12,10 +13,10 @@ function incrementDateBy(number){
   var x = 0;
   var y = 0;
   
-  x = Math.floor(Date/7)
+  x = Math.floor(Date/moneyIncrementInterval)
   Date = Date + number;
   document.getElementById("Date").innerHTML = Date;
-  y = Math.floor(Date/7)
+  y = Math.floor(Date/moneyIncrementInterval)
   console.log(x)
   console.log(y)
   if (y > x){
@@ -25,6 +26,6 @@ function incrementDateBy(number){
 }
 
 window.setInterval(function(){
-incrementDateBy(20);
-},1000);
+incrementDateBy(1);
+},standardInterval);
 
