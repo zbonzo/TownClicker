705ffc3a434e8725668e87df78aa93fa1079d5bc
zbonzo <zbonzo@gmail.com>
2014-07-06 08:22:26 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,23 @@
 var clicks = 0;
+var date = 0;
 
-function incrementby(number){
-clicks = clicks + number;
-document.getElementById("clicks").innerHTML = clicks;
+function incrementmoneyby(number){
+money = money + number;
+document.getElementById("money").innerHTML = money;
 };
 
 
+function incrementdateby(number){
+  x = floor(date/7)
+  date = date + number;
+  document.getElementByID("date").innerHTML = date;
+  y = floor(date/7)
+  console.log(x)
+  console.log(y)
+  
+}
+
 window.setInterval(function(){
-incrementby(1);
+incrementdateby(1);
 },1000);
 
